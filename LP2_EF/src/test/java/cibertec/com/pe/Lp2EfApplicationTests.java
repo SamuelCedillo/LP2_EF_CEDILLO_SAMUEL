55c12f39564173717ca8df871eddc72a4f037569
Samuel Cedillo <alvinetjosam@gmail.com>
2024-10-23 13:27:56 -0500

    cracion de proyecto

---
+++ LP2_EF/src/test/java/cibertec/com/pe/Lp2EfApplicationTests.java
@@ -0,0 +1,13 @@
+package cibertec.com.pe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lp2EfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
