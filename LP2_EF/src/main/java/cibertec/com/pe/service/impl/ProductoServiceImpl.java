0da5fe0438b368dd6c2a9ed1dcd2b92e192cb65e
Samuel Cedillo <alvinetjosam@gmail.com>
2024-10-24 07:03:28 -0500

    creacion de services, impl y repository de Producto

--- LP2_EF/src/main/java/cibertec/com/pe/service/impl/ProductoServiceImpl.java
+++ LP2_EF/src/main/java/cibertec/com/pe/service/impl/ProductoServiceImpl.java
@@ -1,5 +1,28 @@
 package cibertec.com.pe.service.impl;
 
-public class ProductoServiceImpl {
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import cibertec.com.pe.model.ProductoEntity;
+import cibertec.com.pe.repository.ProductoRepository;
+import cibertec.com.pe.service.ProductoService;
+import lombok.RequiredArgsConstructor;
+@Service
+@RequiredArgsConstructor
+public class ProductoServiceImpl implements ProductoService{
+
+	private ProductoRepository productoRepository;
+	@Override
+	public List<ProductoEntity> buscarTodosProductos() {
+		// TODO Auto-generated method stub
+		return productoRepository.findAll();
+	}
+
+	@Override
+	public ProductoEntity buscarProductosPorId(Integer id) {
+		// TODO Auto-generated method stub
+		return productoRepository.findById(id).get();
+	}
 
 }
